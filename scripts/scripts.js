ea60ec613f22268129957548b17c455139cb1c99
Zapcren <rquigley@hotmail.ca>
2024-08-12 21:22:37 -0400

    Cleaned up styles + corrected offset view + prioritized mobile view styles + added nav and smooth scroll function

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,22 @@
-$('#day-night-switch').on("click", function() {
-    if ($('html').is('.light-mode')) {
-        $('html').removeClass('light-mode');
-        $('html').addClass('dark-mode');
-    } else {
-        $('html').removeClass('dark-mode');
-        $('html').addClass('light-mode');
-    }
+$(document).ready(function() {
+    $('#day-night-switch').on("click", function() {
+        if ($('html').is('.light-mode')) {
+            $('html').removeClass('light-mode');
+            $('html').addClass('dark-mode');
+        } else {
+            $('html').removeClass('dark-mode');
+            $('html').addClass('light-mode');
+        }
+    });
+    
+    $('.menu-link').on('click', function(event) {
+        event.preventDefault();
+
+        var target = $(this).data('target');
+
+        $('html, body').animate({
+            scrollTop: $(target).offset().top - 70
+        }, 600);
+    });
+
 });
